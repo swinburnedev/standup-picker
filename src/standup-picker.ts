7646e2f9b613e5097821b6a947659bdf026100cf
swinburnedev <andy@swinburne.dev>
2022-05-13 11:35:10 +0100

    add standup picker function

--- src/standup-picker.ts
+++ src/standup-picker.ts
@@ -3,6 +3,6 @@ import { APIGatewayEvent, Context } from 'aws-lambda';
 export async function handler (event: APIGatewayEvent, context: Context) {
   return {
     statusCode: 200,
-    body: "Hi, I'm a netlfiy function!"
+    body: "Hello, I'm a netlify function!"
   };
 }
